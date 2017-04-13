675bdf4fc6305c8a831d9801140e3369577f102a
Warren Fernandes <warren.f.fernandes@gmail.com>
2017-04-12 21:22:19 -0600

    Prints the values in table format
    Clears the terminal and updates table in-place

--- printer.go
+++ printer.go
@@ -1,9 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+	"strings"
+
+	tm "github.com/buger/goterm"
+)
 
 type ResultsPrinter struct{}
 
 func (p *ResultsPrinter) Print(s Stats) {
-	fmt.Printf("%#v\n", s)
+	// fmt.Printf("%#v\n", s)
+	tm.Clear()
+	tm.MoveCursor(1, 1)
+
+	var headers []string
+	for k, _ := range s {
+		headers = append(headers, k)
+	}
+	sort.Strings(headers)
+
+	var values []string
+	for _, h := range headers {
+		values = append(values, fmt.Sprintf("%d", s[h]))
+	}
+
+	output := tm.NewTable(0, 20, 5, ' ', 0)
+	fmt.Fprintf(output, fmt.Sprintf("%s\n", strings.Join(headers, "\t")))
+	fmt.Fprintf(output, fmt.Sprintf("%s\n", strings.Join(values, "\t")))
+	tm.Println(output)
+	tm.Flush()
 }
