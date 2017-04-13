70c60483b8c9759deae64f165f58544fb2a348a9
Warren Fernandes <warren.f.fernandes@gmail.com>
2017-04-12 22:31:04 -0600

    Prints the output in table format

--- printer.go
+++ printer.go
@@ -2,33 +2,22 @@ package main
 
 import (
 	"fmt"
-	"sort"
-	"strings"
 
 	tm "github.com/buger/goterm"
+	"github.com/olekukonko/tablewriter"
 )
 
 type ResultsPrinter struct{}
 
 func (p *ResultsPrinter) Print(s Stats) {
-	// fmt.Printf("%#v\n", s)
+
 	tm.Clear()
 	tm.MoveCursor(1, 1)
-
-	var headers []string
-	for k, _ := range s {
-		headers = append(headers, k)
+	table := tablewriter.NewWriter(tm.Screen)
+	table.SetHeader([]string{"Name", "Value"})
+	for k, v := range s {
+		table.Append([]string{k, fmt.Sprintf("%d", v)})
 	}
-	sort.Strings(headers)
-
-	var values []string
-	for _, h := range headers {
-		values = append(values, fmt.Sprintf("%d", s[h]))
-	}
-
-	output := tm.NewTable(0, 20, 5, ' ', 0)
-	fmt.Fprintf(output, fmt.Sprintf("%s\n", strings.Join(headers, "\t")))
-	fmt.Fprintf(output, fmt.Sprintf("%s\n", strings.Join(values, "\t")))
-	tm.Println(output)
+	table.Render()
 	tm.Flush()
 }
